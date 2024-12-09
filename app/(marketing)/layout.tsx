a3270f2d349d961a5c34e2d886cf6e9525cace9a
cian-oL <cianoleary2010@gmail.com>
2024-12-09 18:02:59 +0000

    marketing wip

---
+++ app/(marketing)/layout.tsx
@@ -0,0 +1,11 @@
+const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="min-h-screen flex flex-col bg-blue-500">
+      <main className="flex-1 flex flex-col items-center justify-center">
+        {children}
+      </main>
+    </div>
+  );
+};
+
+export default MarketingLayout;
