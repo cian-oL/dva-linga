7af5f2b32d97d550c0a1c3e9f22a2945f3879d0b
cian-oL <cianoleary2010@gmail.com>
2025-01-21 20:11:37 +0000

    changed landing brand colours

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -18,8 +18,13 @@ const Home = () => {
         <Image src="/dva-linga-logo.svg" alt="Dva Linga Logo" fill />
       </div>
       <div className="flex flex-col items-center gap-y-8">
-        <h1 className="text-4xl font-bold text-center text-green-600 tracking-wide">
-          DvaLinga
+        <h1 className="text-4xl font-bold text-center tracking-wide">
+          <span className="bg-gradient-to-b from-[rgb(227,27,28)] from-0% via-[#FFFFFF] via-33% to-[rgb(61,88,219)] to-66% text-transparent font-extrabold bg-clip-text text-stroke-medium">
+            Dva
+          </span>
+          <span className="bg-gradient-to-r from-[rgb(80,160,142)] from-0% via-[#FFFFFF] via-50% to-[rgb(253,134,44)] to-100% text-transparent font-extrabold bg-clip-text text-stroke-medium">
+            Linga
+          </span>
         </h1>
         <h2 className="text-xl text-slate-600 lg:mx-3 lg:text-center">
           Master Croatian with Dva Linga
